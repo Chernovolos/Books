21f346733615f12c3708757b6de97af0b932eba3
unknown <you@example.com>
2020-02-06 21:55:04 +0200

    first commit

---
+++ second.js
@@ -0,0 +1,14 @@
+let x = false;
+
+/*window.onload = () => {
+    document.getElementById("inner").addEventListener("mouseover", (event) => {
+        event.target.classList.add("blue");
+        event.target.classList.remove("pink");
+        
+    });
+    document.getElementById("inner").addEventListener("mouseout", (event) => {
+        event.target.classList.add("pink");
+        event.target.classList.remove("blue");
+    });
+};*/
+
